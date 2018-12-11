c4b65bca90aa7cd54a7b89c5e4052adb6ce23fc5
mario-pe <perkowski.mar@gmail.com>
2018-12-11 18:16:39 +0100

    Publish the all

--- PCK_SBD/settings.py
+++ PCK_SBD/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.urls import reverse_lazy
@@ -23,13 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=#x_-*p@=2hk)2cftsf9a!&kw%mj_z_s6p4#rog)lp))%a^bx*'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pck-sbd.herokuapp.com/']
 
 
 # Application definition
@@ -77,14 +76,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'PCK_SBD.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 AUTH_USER_MODEL = "account.User"
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PCK_SBD.wsgi
