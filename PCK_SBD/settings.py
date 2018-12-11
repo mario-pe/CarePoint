7681de19f0c91b6545f6ada13132347c3fd99131
mario-pe <perkowski.mar@gmail.com>
2018-12-11 20:01:01 +0100

    pub_1.2

--- PCK_SBD/settings.py
+++ PCK_SBD/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['bial-pck.herokuapp.com']
+ALLOWED_HOSTS = ['bial-pck2.herokuapp.com']
 
 
 # Application definition
