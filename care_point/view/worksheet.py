38f0fea3aa461deb9bdb90c1b420350a428ffbbe
mario-pe <perkowski.mar@gmail.com>
2018-12-12 20:39:50 +0100

    poprawa walidacji czasu worksheetów

--- care_point/view/worksheet.py
+++ care_point/view/worksheet.py
@@ -19,21 +19,21 @@ def worksheet_add(request):
     if request.method == 'POST':
         form = WorksheetForm(data=request.POST)
         if form.is_valid():
-            new = form.save(commit=False)
-            caregiver_worksheet_at_date = Worksheet.objects.filter(caregiver=new.caregiver).filter(date=new.date)
-            ward_worksheet_at_date = Worksheet.objects.filter(ward=new.ward).filter(date=new.date)
-            is_caregiver_free = check_available(caregiver_worksheet_at_date, new)
-            is_ward_free = check_available(ward_worksheet_at_date, new)
+            new_worksheet = form.save(commit=False)
+            caregiver_worksheet_at_date = Worksheet.objects.filter(caregiver=new_worksheet.caregiver).filter(date=new_worksheet.date)
+            ward_worksheet_at_date = Worksheet.objects.filter(ward=new_worksheet.ward).filter(date=new_worksheet.date)
+            is_caregiver_free = check_available(caregiver_worksheet_at_date, new_worksheet)
+            is_ward_free = check_available(ward_worksheet_at_date, new_worksheet)
             if is_caregiver_free and is_ward_free:
-                new.save()
+                new_worksheet.save()
                 return redirect('care_point:worksheet')
             elif not is_caregiver_free:
-                info = "W godzinach " + new.hour_from.__str__() + " - " + new.hour_to.__str__() + " pracownik " + new.caregiver.__str__() + " wykonuje inne obowiazki"
-                form = WorksheetForm(data=request.POST, instance=worksheet)
+                info = "W godzinach " + new_worksheet.hour_from.__str__() + " - " + new_worksheet.hour_to.__str__() + " pracownik " + new_worksheet.caregiver.__str__() + " wykonuje inne obowiazki"
+                form = WorksheetForm(data=request.POST, instance=new_worksheet)
                 return render(request, 'care_point/worksheet/worksheet_add.html', {'form': form, "info": info})
             elif not is_ward_free:
-                info = "W godzinach " + new.hour_from.__str__() + " - " + new.hour_to.__str__() + " podopieczny " + new.ward.__str__() + " ma inna wizyte"
-                form = WorksheetForm(data=request.POST, instance=worksheet)
+                info = "W godzinach " + new_worksheet.hour_from.__str__() + " - " + new_worksheet.hour_to.__str__() + " podopieczny " + new_worksheet.ward.__str__() + " ma inna wizyte"
+                form = WorksheetForm(data=request.POST, instance=new_worksheet)
                 return render(request, 'care_point/worksheet/worksheet_add.html', {'form': form, "info": info})
     else:
         form = WorksheetForm()
